8820ea37fcc5da39e9c8596ea762f70c2d579d19
Tathagata Das <tathagata.das1565@gmail.com>
2023-03-05 03:50:18 -0500

    Added missing files

---
+++ standalone/src/main/scala/io/delta/standalone/internal/core/SimpleLogReplayHelper.scala
@@ -0,0 +1,60 @@
+package io.delta.standalone.internal.core
+
+import scala.collection.JavaConverters._
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
+import org.apache.arrow.memory.RootAllocator
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+
+import io.delta.core.internal.utils.CloseableIteratorScala._
+import io.delta.core.internal.utils.CloseableIteratorScalaImpl
+import io.delta.standalone.core.LogReplayHelper
+import io.delta.standalone.data.RowRecord
+import io.delta.standalone.expressions.Expression
+import io.delta.standalone.types.StructType
+import io.delta.standalone.utils.CloseableIterator
+import io.delta.storage.{LocalLogStore, LogStore}
+
+// scalastyle:off println
+class SimpleLogReplayHelper(
+    val logStore: LogStore,
+    val hadoopConf: Configuration) extends LogReplayHelper {
+  def this(hadoopConf: Configuration) = this(new LocalLogStore(hadoopConf), hadoopConf)
+
+  println("Scott > Created SimpleLogReplayHelper")
+
+  val allocator = new RootAllocator
+
+  override def listLogFiles(dir: String): CloseableIterator[String] = {
+    logStore.listFrom(new Path(dir), hadoopConf)
+      .asScalaClosable.mapAsCloseable(_.getPath.toString).asJava
+  }
+
+  override def readParquetFile(
+      filePath: String,
+      readSchema: StructType,
+      pushdownFilters: Array[Expression]): CloseableIterator[RowRecord] = {
+    ArrowParquetReader.readAsRows(filePath, readSchema, allocator)
+  }
+
+  override def readJsonFile(
+      filePath: String,
+      readSchema: StructType): CloseableIterator[RowRecord] = {
+    val iter = logStore.read(new Path(filePath), hadoopConf)
+    new CloseableIteratorScalaImpl[String](iter.asScala, iter.close).mapAsCloseable { jsonText =>
+      SimpleLogReplayHelper.parseJson(jsonText)
+    }.asJava
+  }
+}
+
+object SimpleLogReplayHelper {
+  val mapper = new ObjectMapper
+  // mapper.setSerializationInclusion(Include.NON_ABSENT)
+  // mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false)
+
+  def parseJson(str: String): RowRecord = {
+    new JsonRow(mapper.readTree(str).asInstanceOf[ObjectNode])
+  }
+}
---
+++ standalone/src/main/scala/io/delta/standalone/internal/core/SimpleScanHelper.scala
@@ -0,0 +1,28 @@
+package io.delta.standalone.internal.core
+
+import java.util.TimeZone
+
+import org.apache.arrow.memory.RootAllocator
+
+import io.delta.standalone.core.DeltaScanHelper
+import io.delta.standalone.data.ColumnarRowBatch
+import io.delta.standalone.types.StructType
+import io.delta.standalone.utils.CloseableIterator
+
+// scalastyle:off println
+// NOTE: this cannot take in a val hadoopConf: Configuration, Configuration is not serializable
+class SimpleScanHelper() extends DeltaScanHelper with Serializable {
+  // println("Scott > Created SimpleScanHelper")
+
+  override def readParquetFile(
+      filePath: String,
+      readSchema: StructType,
+      timeZone: TimeZone): CloseableIterator[ColumnarRowBatch] = {
+    // println(s"Scott > SimpleScanHelper > readParquetFile $filePath ${readSchema.getTreeString}")
+    ArrowParquetReader.readAsColumnarBatches(filePath, readSchema, SimpleScanHelper.allocator)
+  }
+}
+
+object SimpleScanHelper {
+  val allocator = new RootAllocator
+}
